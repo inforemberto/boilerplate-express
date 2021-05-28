5abad1fe6dd268f168a052a0206aa28ad353d175
Jorge Remberto Rodriguez Rodriguez <inforemberto@gmail.com>
2021-05-28 18:18:42 -0400

    serve static files and json

--- myApp.js
+++ myApp.js
@@ -1,39 +1,20 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 
+//Serve static files from /public
+let staticPath = __dirname + "/public";
+app.use("/public", express.static(staticPath));
 
+//Index page
+app.get("/", (req, res) => {
+  let absolutePath = __dirname + "/views/index.html";
+  res.sendFile(absolutePath);
+});
 
+app.get("/json", (req, res)=>{
+    res.json({
+        "message": "Hello json"
+    });
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
